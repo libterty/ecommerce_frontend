bcd69e02f267f2371645c31e74b8020288d83ce0
dreak603 <dreak603@gmail.com>
2019-12-11 21:27:19 +0800

    finish HomeCardImage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <b-container>
-    <div class="Home">Hello Home</div>
+    <div>
+      <b-card
+        overlay
+        img-src="https://i.imgur.com/d4C2lls.jpg"
+        img-alt="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
+        text-variant="white"
+        title="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
+        class="Home-Advertisement"
+      >
+        <b-card-text>
+          Christmas Sale is coming ~
+        </b-card-text>
+        <br />
+        <b-button pill size="lg" variant="primary">Explore More</b-button>
+      </b-card>
+    </div>
   </b-container>
 </template>
 
@@ -22,3 +37,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.Home-Advertisement {
+  border: none;
+}
+</style>
