274c5d34d8cc76360e28c97fed1d9cb17b59b1ea
mina <mp922352612@gmail.com>
2019-12-14 00:12:41 +0800

    Revert "Add ShoppingCart component"
    This reverts commit 2b3c73a3b97c3d32949cf1dbb839d26e49611dd7.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from './router';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import vuetify from '@/plugins/vuetify'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,55 @@
 <template>
-  <div>
-    <b-container>
-      <div>
-        <b-card
-          overlay
-          img-src="https://i.imgur.com/JUpkmDj.jpg?1"
-          img-alt="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
-          text-variant="white"
-          title="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
-          class="Home-Advertisement"
-        >
-          <b-card-text>Christmas Sale is coming ~</b-card-text>
-          <br />
-          <b-button
-            pill
-            size="lg"
-            variant="primary"
-          >Explore More</b-button>
-        </b-card>
-      </div>
-
-      <br />
-
-      <HomeCarousel :initCarousels="initCarousels" />
-      <br />
-      <ShoppingCart />
-      <HomeGrid :initProducts="initProducts" />
-    </b-container>
-  </div>
+  <b-container>
+    <div>
+      <b-card
+        overlay
+        img-src="https://i.imgur.com/JUpkmDj.jpg?1"
+        img-alt="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
+        text-variant="white"
+        title="EVERYTHING YOU NEED IN TIME FOR CHRISTMAS"
+        class="Home-Advertisement"
+      >
+        <b-card-text>
+          Christmas Sale is coming ~
+        </b-card-text>
+        <br />
+        <b-button pill size="lg" variant="primary">Explore More</b-button>
+      </b-card>
+    </div>
+    <br />
+    <HomeCarousel :initCarousels="initCarousels" />
+    <br />
+    <HomeGrid :initProducts="initProducts" />
+  </b-container>
 </template>
 
 <script>
-import HomeCarousel from '../components/HomeCarousel.vue'
-import HomeGrid from '../components/HomeGrid'
-import ShoppingCart from '../components/ShoppingCart'
-import Request from '../api/index'
-const request = new Request()
+import HomeCarousel from '../components/HomeCarousel.vue';
+import HomeGrid from '../components/HomeGrid';
+import Request from '../api/index';
+const request = new Request();
 
 export default {
   components: {
     HomeCarousel,
-    HomeGrid,
-    ShoppingCart
+    HomeGrid
   },
   data() {
     return {
       initProducts: [],
       initCarousels: []
-    }
+    };
   },
   async created() {
-    const res = await request.getHomePageProduts()
+    const res = await request.getHomePageProduts();
     if (res.status === 'success') {
-      this.initProducts = res.products
-      for (let i = 0; i < 3; i++) {
-        this.initCarousels.push(this.initProducts[i])
+      this.initProducts = res.products;
+      for (let i=0; i < 3; i++) {
+        this.initCarousels.push(this.initProducts[i]);
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
