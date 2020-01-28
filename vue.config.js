dbac107d515f85fcb9f4db4f31cf2578ee694987
dreak603 <dreak603@gmail.com>
2020-01-28 16:34:04 +0800

    compressing build file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css/,
+            threshold: 8192,
+            deleteOriginalAssets: true
+          })
+        ]
+      }
+    }
+  }
 };
