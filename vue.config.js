0d9fcae06de34c6e6a24e96e3e936e523718f1b7
dreak603 <dreak603@gmail.com>
2020-01-31 03:13:45 +0800

    revise webpack and adding preload for compile file

--- vue.config.js
+++ vue.config.js
@@ -11,27 +11,34 @@ module.exports = {
     config.plugins.delete('prefetch');
     config.plugin('CompressionPlugin').use(CompressionPlugin);
   },
-  configureWebpack: config => {
+  configureWebpack: () => {
     if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        new BundleAnalyzerPlugin(),
-        new CompressionPlugin({
-          filename: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-          threshold: 8192,
-          minRatio: 0.8
-        }),
-        new prerenderSpaPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: [
-            '/',
-            '/furnitures',
-            '/signin',
-            '/signup'
-          ]
-        })
-      );
+      return {
+        plugins: [
+          new BundleAnalyzerPlugin(),
+          new CompressionPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            threshold: 8192,
+            minRatio: 0.8
+          }),
+          new prerenderSpaPlugin({
+            staticDir: path.join(__dirname, 'dist'),
+            routes: [
+              '/',
+              '/furnitures',
+              '/signin',
+              '/signup'
+            ]
+          })
+        ],
+        resolve: {
+          alias: {
+            vue$: 'vue/dist/vue.runtime.esm.js'
+          }
+        }
+      }
     }
   }
 };
