b0922162312b5841b96989e66bb9f26d5e9cc1a3
minaH <mp922352612@gmail.com>
2020-02-04 22:03:16 +0800

    Fix Orders page to be RWD

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -2,119 +2,60 @@
   <v-app class="py-5 mx-5">
     <v-container>
       <UserTabs />
-
       <v-row no-gutters>
         <v-col
           cols="12"
           lg="12"
         >
-          <v-card class="cart py-5 text--darken-3 cyan--text">
+          <v-card class="pt-5">
             <v-col
               cols="12"
               lg="12"
-              class="my-4"
             >
-              <span class="cart-title display-1">|Check Orders|</span>
-              <v-card-title
-                class="text-muted mb-5 headline"
-                v-if="orders.length<1"
-              >No Data</v-card-title>
-            </v-col>
-
-            <v-row
-              justify="center"
-              align="center"
-              class="ml-1"
-            >
-              <v-col
-                cols="12"
-                lg="10"
-              >
-                <v-row
-                  justify="center"
-                  align="center"
-                  class="ml-10"
+              <div class="my-4">
+                <span class="order-title display-1 py-5">|Check Orders|</span>
+                <v-card-title
+                  class="text-muted mb-5 headline"
+                  v-if="orders.length<1"
+                >No Data</v-card-title>
+                <v-simple-table
+                  class="mt-5"
+                  v-else
                 >
-                  <v-col
-                    cols="12"
-                    lg="3"
-                  >
-                    <v-card-title class="text-muted mb-5 title">Order Serial</v-card-title>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    lg="3"
-                  >
-                    <v-card-title class="text-muted mb-5 title">Total Price</v-card-title>
-                  </v-col>
-
-                  <v-col
-                    cols="12"
-                    lg="3"
-                  >
-                    <v-card-title class="text-muted mb-5 title">Created Time</v-card-title>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    lg="3"
-                  >
-                    <v-card-title class="text-muted mb-5 title">Shipping Status</v-card-title>
-                  </v-col>
-                </v-row>
-
-                <v-list flat>
-                  <v-list-item-group>
-                    <v-list-item
-                      v-for="order in orders"
-                      :key="order.id"
-                    >
-                      <v-row
-                        justify="center"
-                        align="center"
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th class="text-center">Order Serial</th>
+                        <th class="text-center">Total Price</th>
+                        <th class="text-center">Created Time</th>
+                        <th class="text-center">Shipping Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="order in orders"
+                        :key="order.id"
                       >
-                        <v-col
-                          cols="12"
-                          lg="3"
-                        >
-                          <v-list-item-title class="mb-1">{{order.sn}}</v-list-item-title>
-                        </v-col>
-                        <v-col
-                          cols="12"
-                          lg="3"
-                        >
-                          <v-card-text
-                            class="text--darken-3 cyan--text title"
-                          >$ {{order.total_amount}}</v-card-text>
-                        </v-col>
-
-                        <v-col
-                          cols="12"
-                          lg="3"
-                        >
-                          <v-card-text class="body-1">{{order.createdAt |convertTime}}</v-card-text>
-                        </v-col>
-                        <v-col
-                          cols="12"
-                          lg="3"
-                        >
-                          <v-card-text class="body-1 mt-2">{{order.shipping_status}}</v-card-text>
-                          <v-list-item-action>
-                            <v-btn
-                              raised
-                              color="error"
-                              small
-                              v-if="order.shipping_status==='未出貨'"
-                              class="red lighten-3"
-                              :to="{name:'order',params:{orderId:order.id,userId:order.UserId}}"
-                            >Checkout</v-btn>
-                          </v-list-item-action>
-                        </v-col>
-                      </v-row>
-                    </v-list-item>
-                  </v-list-item-group>
-                </v-list>
-              </v-col>
-            </v-row>
+                        <td>{{ order.sn }}</td>
+                        <td>{{ order.total_amount }}</td>
+                        <td>{{ order.createdAt |convertTime }}</td>
+                        <td>{{ order.shipping_status }}</td>
+                        <td>
+                          <v-btn
+                            raised
+                            color="error"
+                            small
+                            v-if="order.shipping_status==='未出貨'"
+                            class="red lighten-3"
+                            :to="{name:'order',params:{orderId:order.id,userId:order.UserId}}"
+                          >Checkout</v-btn>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </div>
+            </v-col>
           </v-card>
         </v-col>
       </v-row>
@@ -183,5 +124,10 @@ export default {
 <style scoped>
 * {
   font-family: sans-serif, 'Noto Sans TC', 'Roboto';
+}
+.order-title {
+  color: #42602d;
+  background-color: white;
+  padding: 0 0.5em;
 }
 </style>
