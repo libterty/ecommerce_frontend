4a8b4a3e510bc4f5600e11c996c87090ab882ae5
minaH <mp922352612@gmail.com>
2020-02-10 00:35:36 +0800

    Change furniture serach looking

--- src/components/FurnitureSearch.vue
+++ src/components/FurnitureSearch.vue
@@ -1,56 +1,90 @@
 <template>
-    <div class="col-md-6 col-lg-4">
-        <b-card-group deck>
-            <b-card
-                :title="initSearchProuct.name"
-                :sub-title="initSearchProuct.Category.name"
-                tag="article"
-                class="mb-2"
-            >
-                <b-card-img-lazy :src="initSearchProuct.Images[0].url" :alt="initSearchProuct.name" top></b-card-img-lazy>
-                <b-card-text>{{ initSearchProuct.description | shortenDesc }}</b-card-text>
-                <template v-slot:footer>
-                    <b-button-group>
-                        <b-button :href="'/furnitures/'+ initSearchProuct.id" variant="primary">Visit {{initSearchProuct.name}}</b-button>
-                        <b-button variant="primary" disabled>
-                            關注度
-                            <b-badge variant="light">{{initSearchProuct.viewCounts + 1}}</b-badge>
-                        </b-button>
-                    </b-button-group>
-                </template>
-            </b-card>
-        </b-card-group>
-    </div>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        cols="12"
+        lg="4"
+        v-for="initSearchProduct in initSearchProducts"
+        :key="initSearchProduct.id"
+      >
+        <v-card
+          min-width="300"
+          max-width="300"
+          class="text-left remove-link"
+          link
+          :to="{name:'furnituresItem',params:{id:initSearchProduct.id}}"
+        >
+          <v-img
+            height="300px"
+            :src="initSearchProduct.Images[0].url"
+            :alt="initSearchProduct.name"
+          ></v-img>
+
+          <v-card-title class="cyan--text text--darken-4">{{initSearchProduct.name}}</v-card-title>
+          <v-card-text>{{initSearchProduct.Category.name}}</v-card-text>
+          <v-card-actions>
+            <v-rating
+              x-small
+              class="p-2"
+              color="cyan darken-4"
+              background-color="cyan darken-4"
+              half-increments
+              :value="initSearchProduct.rating/2"
+              readonly
+            ></v-rating>
+            <v-chip
+              label
+              class="mt-3 red lighten-3 white--text"
+              :to="{name:'furnituresItem',params:{id:initSearchProduct.id}}"
+            >${{initSearchProduct.price}}</v-chip>
+            <v-badge color="red">
+              <span slot="badge">{{initSearchProduct.viewCounts + 1}}</span>
+              <v-icon
+                large
+                color="red"
+                v-if="initSearchProduct.viewCounts > 20"
+              >mdi-fire</v-icon>
+              <v-icon
+                large
+                color="grey"
+                v-else
+              >mdi-fire</v-icon>
+            </v-badge>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    props: {
-        initSearchProuct: {
-            type: Object
-        }
-    },
-    filters: {
-        shortenDesc(d) {
-            if (!d) return 'No description yet';
-            return d.substring(0, 50);
-        }
+  props: {
+    initSearchProducts: {
+      type: Object
     }
+  },
+  filters: {
+    shortenDesc(d) {
+      if (!d) return 'No description yet'
+      return d.substring(0, 50)
+    }
+  }
 }
 </script>
 
 <style scoped>
 img {
-    max-height: 203px;
-    max-width: 246px;
-    width: auto;
+  max-height: 203px;
+  max-width: 246px;
+  width: auto;
 }
 p.card-text {
-    font-size: small;
-    white-space: nowrap;
-    overflow: scroll;
+  font-size: small;
+  white-space: nowrap;
+  overflow: scroll;
 }
 a.btn.btn-primary {
-    color: #cccccc;
+  color: #cccccc;
 }
 </style>
